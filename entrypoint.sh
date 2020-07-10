60e3e6b47fb495df73655af90b3ae55fe646c936
ωξβ <lbonanomi@gmail.com>
2020-07-10 19:40:09 -0400

    count

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,25 +15,25 @@ done
 which base64
 
 export LANG1_NAME=$(sort -rnk2 STATS | head -1 | awk '{ print $1 }')
-export LANG1_BYTES=$(sort -rnk2 STATS | head -1 | awk '{ print $2 }')
+export LANG1_BYTES=$(sort -rnk2 STATS | head -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 export LANG2_NAME=$(sort -rnk2 STATS | head -2 | tail -1 | awk '{ print $1 }')
-export LANG2_BYTES=$(sort -rnk2 STATS | head -2 | tail -1 | awk '{ print $2 }')
+export LANG2_BYTES=$(sort -rnk2 STATS | head -2 | tail -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 export LANG3_NAME=$(sort -rnk2 STATS | head -3 | tail -1 | awk '{ print $1 }')
-export LANG3_BYTES=$(sort -rnk2 STATS | head -3 | tail -1 | awk '{ print $2 }')
+export LANG3_BYTES=$(sort -rnk2 STATS | head -3 | tail -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 export LANG4_NAME=$(sort -rnk2 STATS | head -4 | tail -1 | awk '{ print $1 }')
-export LANG4_BYTES=$(sort -rnk2 STATS | head -4 | tail -1 | awk '{ print $2 }')
+export LANG4_BYTES=$(sort -rnk2 STATS | head -4 | tail -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 export LANG5_NAME=$(sort -rnk2 STATS | head -5 | tail -1 | awk '{ print $1 }')
-export LANG5_BYTES=$(sort -rnk2 STATS | head -5 | tail -1 | awk '{ print $2 }')
+export LANG5_BYTES=$(sort -rnk2 STATS | head -5 | tail -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 export LANG6_NAME=$(sort -rnk2 STATS | head -6 | tail -1 | awk '{ print $1 }')
-export LANG6_BYTES=$(sort -rnk2 STATS | head -6 | tail -1 | awk '{ print $2 }')
+export LANG6_BYTES=$(sort -rnk2 STATS | head -6 | tail -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 export LANG7_NAME=$(sort -rnk2 STATS | head -7 | tail -1 | awk '{ print $1 }')
-export LANG7_BYTES=$(sort -rnk2 STATS | head -7 | tail -1 | awk '{ print $2 }')
+export LANG7_BYTES=$(sort -rnk2 STATS | head -7 | tail -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
 
 cat template.svg | envsubst | base64 > label.svg
