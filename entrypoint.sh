d18d4ddbfd1481c660c02bec85aa5f3ebf278d9e
ωξβ <lbonanomi@gmail.com>
2020-07-10 15:29:56 -0400

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+
+REPO_COUNT=$(curl -s https://api.github.com/users/$1/repos | jq .[].full_name | wc -l)
+echo "Hello $1, you have $REPO_COUNT repos under your name"
 time=$(date)
 echo "::set-output name=time::$time"
 
