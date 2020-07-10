d63342700c2a30f6e1d97b905aa0748da71033e6
ωξβ <lbonanomi@gmail.com>
2020-07-10 16:13:54 -0400

    count

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -l
 
-
 REPO_COUNT=$(curl -s https://api.github.com/users/$1/repos | jq .[].full_name | wc -l)
+
+curl -s https://api.github.com/users/$1/repos | jq .[].languages_url | while read dump
+do
+	echo curl -s $dump | awk '/:/ { gsub(/\"/,"");gsub(/:/,"");gsub(/,/,""); print;}' 
+done > BUFF
+
+awk '{ print $1 }' BUFF | sort | uniq | while read uniq_lang
+do
+	awk '/'"$uniq_lang"'/ { a=a+$2 } END { print '"$uniq_lang"', a }'
+done
+
 echo "Hello $1, you have $REPO_COUNT repos under your name"
-time=$(date)
-echo "::set-output name=time::$time"
+
 
 
